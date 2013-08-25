d59200f8a6dec846138ed61dfb0d3993fa124a68
Atsushi Odagiri <aodagx@gmail.com>
2013-08-25 17:43:54 +0900

    reduce support versions

--- setup.py
+++ setup.py
@@ -33,10 +33,8 @@ setup(name='rebecca.view',
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
